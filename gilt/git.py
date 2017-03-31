TieWei/gilt

Speed up git operations by multi-threads
Gilt clone and extract/overlay git resources in sequence, it's
slow when there's a lot gilt resources defined in config. In this
commit, it uses multi-threading to speed up these operations, in
order to make the thread operations safe, it uses git environment
variables instead of chdir into the repo clone/branched.

gilt/git.py
@@ -20,6 +20,7 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
+import fasteners
 import glob
 import os
 import shutil
@@ -27,6 +28,37 @@ import shutil
 import sh
 
 from gilt import util
+
+
+def _git_env(repository):
+    """
+    Git environment variables for specific repository.
+
+    :param repository: path to a git repository.
+    :return: dict of envionment variables.
+    """
+    return {
+        "GIT_WORK_TREE": repository,
+        "GIT_DIR": os.path.join(repository, ".git"),
+        "GIT_OBJECT_DIRECTORY": os.path.join(repository, ".git", "objects")
+    }
+
+
+def do_overlay(config, debug=False):
+    """
+    Actual methods the overlays git based repo.
+
+    :param config: A dict contains configuration of the git resource.
+    :param debug: An optional bool to toggle debug output.
+    :return: None
+    """
+    with fasteners.InterProcessLock(config.lock_file):
+        if not os.path.exists(config.src):
+            clone(config.name, config.git, config.src, debug=debug)
+        if config.dst:
+            extract(config.src, config.dst, config.version, debug=debug)
+        else:
+            overlay(config.src, config.files, config.version, debug=debug)
 
 
 def clone(name, repository, destination, debug=False):
@@ -60,15 +92,18 @@ def extract(repository, destination, version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: None
     """
-    with util.saved_cwd():
-        os.chdir(repository)
-        _get_branch(version, debug)
-        cmd = sh.git.bake(
-            'checkout-index', force=True, all=True, prefix=destination)
-        util.run_command(cmd, debug=debug)
-        msg = '  - extracting ({}) {} to {}'.format(version, repository,
-                                                    destination)
-        util.print_info(msg)
+    git_env = _git_env(repository)
+    _get_branch(version, debug, git_env)
+    cmd = sh.git.bake(
+        'checkout-index',
+        force=True,
+        all=True,
+        prefix=destination,
+        _env=git_env)
+    util.run_command(cmd, debug=debug)
+    msg = '  - extracting ({}) {} to {}'.format(version, repository,
+                                                destination)
+    util.print_info(msg)
 
 
 def overlay(repository, files, version, debug=False):
@@ -83,27 +118,26 @@ def overlay(repository, files, version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: None
     """
-    with util.saved_cwd():
-        os.chdir(repository)
-        _get_branch(version, debug)
 
-        for fc in files:
-            if '*' in fc.src:
-                for filename in glob.glob(fc.src):
-                    util.copy(filename, fc.dst)
-                    msg = '  - copied ({}) {} to {}'.format(version, filename,
-                                                            fc.dst)
-                    util.print_info(msg)
-            else:
-                if os.path.isdir(fc.dst) and os.path.isdir(fc.src):
-                    shutil.rmtree(fc.dst)
-                util.copy(fc.src, fc.dst)
-                msg = '  - copied ({}) {} to {}'.format(version, fc.src,
+    git_env = _git_env(repository)
+    _get_branch(version, debug, git_env)
+
+    for fc in files:
+        if '*' in fc.src:
+            for filename in glob.glob(fc.src):
+                util.copy(filename, fc.dst)
+                msg = '  - copied ({}) {} to {}'.format(version, filename,
                                                         fc.dst)
                 util.print_info(msg)
+        else:
+            if os.path.isdir(fc.dst) and os.path.isdir(fc.src):
+                shutil.rmtree(fc.dst)
+            util.copy(fc.src, fc.dst)
+            msg = '  - copied ({}) {} to {}'.format(version, fc.src, fc.dst)
+            util.print_info(msg)
 
 
-def _get_branch(version, debug=False):
+def _get_branch(version, debug=False, env=None):
     """
     Handle switching to the specified version and return None.
 
@@ -116,18 +150,18 @@ def _get_branch(version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: None
     """
-    cmd = sh.git.bake('fetch')
+    cmd = sh.git.bake('fetch', _env=env)
     util.run_command(cmd, debug=debug)
-    cmd = sh.git.bake('checkout', version)
+    cmd = sh.git.bake('checkout', version, _env=env)
     util.run_command(cmd, debug=debug)
-    cmd = sh.git.bake('clean', '-d', '-x', '-f')
+    cmd = sh.git.bake('clean', '-d', '-x', '-f', _env=env)
     util.run_command(cmd, debug=debug)
-    if _is_branch(version, debug):
-        cmd = sh.git.bake('pull', rebase=True, ff_only=True)
+    if _is_branch(version, debug, env):
+        cmd = sh.git.bake('pull', rebase=True, ff_only=True, _env=env)
         util.run_command(cmd, debug=debug)
 
 
-def _is_branch(version, debug=False):
+def _is_branch(version, debug=False, env=None):
     """
     Determine a version is a git branch name or not.
 
@@ -135,8 +169,12 @@ def _is_branch(version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: bool
     """
-    cmd = sh.git.bake('show-ref', '--verify', '--quiet',
-                      "refs/heads/{}".format(version))
+    cmd = sh.git.bake(
+        'show-ref',
+        '--verify',
+        '--quiet',
+        "refs/heads/{}".format(version),
+        _env=env)
     try:
         util.run_command(cmd, debug=debug)
         return True

test/test_git.py
@@ -145,16 +145,42 @@ def test_get_branch(mocker, patched_run_command):
     assert expected == patched_run_command.mock_calls
 
 
+def test_get_branch_with_env(mocker, patched_run_command):
+    git_env = {"foo", "bar"}
+    git._get_branch('branch', env=git_env)
+    # yapf: disable
+    expected = [
+        mocker.call(sh.git.bake('fetch', _env=git_env), debug=False),
+        mocker.call(sh.git.bake('checkout', 'branch', _env=git_env),
+                    debug=False),
+        mocker.call(sh.git.bake('clean', '-d', '-x', '-f', _env=git_env),
+                    debug=False),
+        mocker.call(sh.git.bake('show-ref', '--verify', '--quiet',
+                                'refs/heads/branch', _env=git_env),
+                    debug=False),
+        mocker.call(sh.git.bake('pull', rebase=True, ff_only=True,
+                                _env=git_env),
+                    debug=False)
+    ]
+    # yapf: enable
+
+    assert expected == patched_run_command.mock_calls
+
+
 @slow
 def test_is_branch(temp_dir):
     name = 'retr0h.ansible-etcd'
     repo = 'https://github.com/retr0h/ansible-etcd.git'
     destination = os.path.join(temp_dir.strpath, name)
     git.clone(name, repo, destination)
-    os.chdir(destination)
-    assert git._is_branch('master')
-    assert not git._is_branch('1.1')
-    assert not git._is_branch('888ef7b')
+    git_env = {
+        "GIT_WORK_TREE": destination,
+        "GIT_DIR": os.path.join(destination, ".git"),
+        "GIT_OBJECT_DIRECTORY": os.path.join(destination, ".git", "objects")
+    }
+    assert git._is_branch('master', env=git_env)
+    assert not git._is_branch('1.1', env=git_env)
+    assert not git._is_branch('888ef7b', env=git_env)
 
 
 def test_is_branch_nonbranch(mocker, patched_run_command):
@@ -169,3 +195,73 @@ def test_is_branch_nonbranch(mocker, patched_run_command):
     # yapf: enable
 
     assert expected == patched_run_command.mock_calls
+
+
+def test_git_env():
+    result = git._git_env("temp_dir")
+
+    assert result == {
+        "GIT_WORK_TREE": "temp_dir",
+        "GIT_DIR": os.path.join("temp_dir", ".git"),
+        "GIT_OBJECT_DIRECTORY": os.path.join("temp_dir", ".git", "objects")
+    }
+
+
+def test_do_overlay_with_clone_extract(mocker, tmpdir):
+    clone = mocker.patch("gilt.git.clone")
+    extract = mocker.patch("gilt.git.extract")
+    overlay = mocker.patch("gilt.git.overlay")
+    config = mocker.MagicMock()
+    config.dst = "dest_dir"
+    mocker.patch("fasteners.InterProcessLock").return_value = \
+        mocker.MagicMock()
+    mocker.patch("os.path.exists").return_value = False
+    git.do_overlay(config)
+    clone.assert_called_once()
+    extract.assert_called_once()
+    overlay.assert_not_called()
+
+
+def test_do_overlay_with_clone_overlay(mocker, tmpdir):
+    clone = mocker.patch("gilt.git.clone")
+    extract = mocker.patch("gilt.git.extract")
+    overlay = mocker.patch("gilt.git.overlay")
+    config = mocker.MagicMock()
+    config.dst = ""
+    mocker.patch("fasteners.InterProcessLock").return_value = \
+        mocker.MagicMock()
+    mocker.patch("os.path.exists").return_value = False
+    git.do_overlay(config)
+    clone.assert_called_once()
+    extract.assert_not_called()
+    overlay.assert_called_once()
+
+
+def test_do_overlay_with_noclone_extract(mocker, tmpdir):
+    clone = mocker.patch("gilt.git.clone")
+    extract = mocker.patch("gilt.git.extract")
+    overlay = mocker.patch("gilt.git.overlay")
+    config = mocker.MagicMock()
+    config.dst = "dest_dir"
+    mocker.patch("fasteners.InterProcessLock").return_value = \
+        mocker.MagicMock()
+    mocker.patch("os.path.exists").return_value = True
+    git.do_overlay(config)
+    clone.assert_not_called()
+    extract.assert_called_once()
+    overlay.assert_not_called()
+
+
+def test_do_overlay_with_noclone_overlay(mocker, tmpdir):
+    clone = mocker.patch("gilt.git.clone")
+    extract = mocker.patch("gilt.git.extract")
+    overlay = mocker.patch("gilt.git.overlay")
+    config = mocker.MagicMock()
+    config.dst = ""
+    mocker.patch("fasteners.InterProcessLock").return_value = \
+        mocker.MagicMock()
+    mocker.patch("os.path.exists").return_value = True
+    git.do_overlay(config)
+    clone.assert_not_called()
+    extract.assert_not_called()
+    overlay.assert_called_once()

retr0h/py-gilt

gilt/git.py
@@ -20,6 +20,7 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
+import fasteners
 import glob
 import os
 import shutil
@@ -27,6 +28,37 @@ import shutil
 import sh
 
 from gilt import util
+
+
+def _git_env(repository):
+    """
+    Git environment variables for specific repository.
+
+    :param repository: path to a git repository.
+    :return: dict of envionment variables.
+    """
+    return {
+        "GIT_WORK_TREE": repository,
+        "GIT_DIR": os.path.join(repository, ".git"),
+        "GIT_OBJECT_DIRECTORY": os.path.join(repository, ".git", "objects")
+    }
+
+
+def do_overlay(config, debug=False):
+    """
+    Actual methods the overlays git based repo.
+
+    :param config: A dict contains configuration of the git resource.
+    :param debug: An optional bool to toggle debug output.
+    :return: None
+    """
+    with fasteners.InterProcessLock(config.lock_file):
+        if not os.path.exists(config.src):
+            clone(config.name, config.git, config.src, debug=debug)
+        if config.dst:
+            extract(config.src, config.dst, config.version, debug=debug)
+        else:
+            overlay(config.src, config.files, config.version, debug=debug)
 
 
 def clone(name, repository, destination, debug=False):
@@ -60,15 +92,18 @@ def extract(repository, destination, version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: None
     """
-    with util.saved_cwd():
-        os.chdir(repository)
-        _get_version(version, debug)
-        cmd = sh.git.bake(
-            'checkout-index', force=True, all=True, prefix=destination)
-        util.run_command(cmd, debug=debug)
-        msg = '  - extracting ({}) {} to {}'.format(version, repository,
-                                                    destination)
-        util.print_info(msg)
+    git_env = _git_env(repository)
+    _get_version(version, debug, git_env)
+    cmd = sh.git.bake(
+        'checkout-index',
+        force=True,
+        all=True,
+        prefix=destination,
+        _env=git_env)
+    util.run_command(cmd, debug=debug)
+    msg = '  - extracting ({}) {} to {}'.format(version, repository,
+                                                destination)
+    util.print_info(msg)
 
 
 def overlay(repository, files, version, debug=False):
@@ -83,27 +118,25 @@ def overlay(repository, files, version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: None
     """
-    with util.saved_cwd():
-        os.chdir(repository)
-        _get_version(version, debug)
+    git_env = _git_env(repository)
+    _get_version(version, debug, git_env)
 
-        for fc in files:
-            if '*' in fc.src:
-                for filename in glob.glob(fc.src):
-                    util.copy(filename, fc.dst)
-                    msg = '  - copied ({}) {} to {}'.format(
-                        version, filename, fc.dst)
-                    util.print_info(msg)
-            else:
-                if os.path.isdir(fc.dst) and os.path.isdir(fc.src):
-                    shutil.rmtree(fc.dst)
-                util.copy(fc.src, fc.dst)
+    for fc in files:
+        if '*' in fc.src:
+            for filename in glob.glob(fc.src):
+                util.copy(filename, fc.dst)
                 msg = '  - copied ({}) {} to {}'.format(
-                    version, fc.src, fc.dst)
+                    version, filename, fc.dst)
                 util.print_info(msg)
+        else:
+            if os.path.isdir(fc.dst) and os.path.isdir(fc.src):
+                shutil.rmtree(fc.dst)
+            util.copy(fc.src, fc.dst)
+            msg = '  - copied ({}) {} to {}'.format(version, fc.src, fc.dst)
+            util.print_info(msg)
 
 
-def _get_version(version, debug=False):
+def _get_version(version, debug=False, env=None):
     """
     Handle switching to the specified version and return None.
 
@@ -116,21 +149,20 @@ def _get_version(version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: None
     """
-    if not any(
-        (_has_branch(version, debug), _has_tag(version, debug), _has_commit(
-            version, debug))):
-        cmd = sh.git.bake('fetch')
+    if not any((_has_branch(version, debug, env), _has_tag(
+            version, debug, env), _has_commit(version, debug, env))):
+        cmd = sh.git.bake('fetch', _env=env)
         util.run_command(cmd, debug=debug)
-    cmd = sh.git.bake('checkout', version)
+    cmd = sh.git.bake('checkout', version, _env=env)
     util.run_command(cmd, debug=debug)
-    cmd = sh.git.bake('clean', '-d', '-x', '-f')
+    cmd = sh.git.bake('clean', '-d', '-x', '-f', _env=env)
     util.run_command(cmd, debug=debug)
     if _has_branch(version, debug):
-        cmd = sh.git.bake('pull', rebase=True, ff_only=True)
+        cmd = sh.git.bake('pull', rebase=True, ff_only=True, _env=env)
         util.run_command(cmd, debug=debug)
 
 
-def _has_commit(version, debug=False):
+def _has_commit(version, debug=False, env=None):
     """
     Determine a version is a local git commit sha or not.
 
@@ -138,9 +170,9 @@ def _has_commit(version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: bool
     """
-    if _has_tag(version, debug) or _has_branch(version, debug):
+    if _has_branch(version, debug, env) or _has_tag(version, debug, env):
         return False
-    cmd = sh.git.bake('cat-file', '-e', version)
+    cmd = sh.git.bake('cat-file', '-e', version, _env=env)
     try:
         util.run_command(cmd, debug=debug)
         return True
@@ -148,7 +180,7 @@ def _has_commit(version, debug=False):
         return False
 
 
-def _has_tag(version, debug=False):
+def _has_tag(version, debug=False, env=None):
     """
     Determine a version is a local git tag name or not.
 
@@ -156,8 +188,12 @@ def _has_tag(version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: bool
     """
-    cmd = sh.git.bake('show-ref', '--verify', '--quiet',
-                      "refs/tags/{}".format(version))
+    cmd = sh.git.bake(
+        'show-ref',
+        '--verify',
+        '--quiet',
+        "refs/tags/{}".format(version),
+        _env=env)
     try:
         util.run_command(cmd, debug=debug)
         return True
@@ -165,7 +201,7 @@ def _has_tag(version, debug=False):
         return False
 
 
-def _has_branch(version, debug=False):
+def _has_branch(version, debug=False, env=None):
     """
     Determine a version is a local git branch name or not.
 
@@ -173,8 +209,12 @@ def _has_branch(version, debug=False):
     :param debug: An optional bool to toggle debug output.
     :return: bool
     """
-    cmd = sh.git.bake('show-ref', '--verify', '--quiet',
-                      "refs/heads/{}".format(version))
+    cmd = sh.git.bake(
+        'show-ref',
+        '--verify',
+        '--quiet',
+        "refs/heads/{}".format(version),
+        _env=env)
     try:
         util.run_command(cmd, debug=debug)
         return True

test/test_git.py
@@ -159,6 +159,36 @@ def test_get_version_has_tag(mocker, patched_run_command):
     assert expected == patched_run_command.mock_calls
 
 
+def test_get_version_with_env(mocker, patched_run_command):
+    git_env = {"foo", "bar"}
+    git._get_version('branch', env=git_env)
+    # yapf: disable
+    expected = [
+        mocker.call(sh.git.bake('show-ref', '--verify', '--quiet',
+                                'refs/heads/branch', _env=git_env),
+                    debug=False),
+        mocker.call(sh.git.bake('show-ref', '--verify', '--quiet',
+                                'refs/tags/branch', _env=git_env),
+                    debug=False),
+        mocker.call(sh.git.bake('show-ref', '--verify', '--quiet',
+                                'refs/heads/branch', _env=git_env),
+                    debug=False),
+        mocker.call(sh.git.bake('checkout', 'branch', _env=git_env),
+                    debug=False),
+        mocker.call(sh.git.bake('clean', '-d', '-x', '-f', _env=git_env),
+                    debug=False),
+        mocker.call(sh.git.bake('show-ref', '--verify', '--quiet',
+                                'refs/heads/branch', _env=git_env),
+                    debug=False),
+        mocker.call(sh.git.bake('pull', rebase=True, ff_only=True,
+                                _env=git_env),
+                    debug=False)
+    ]
+    # yapf: enable
+
+    assert expected == patched_run_command.mock_calls
+
+
 def test_get_version_has_commit(mocker, patched_run_command):
     mocker.patch('gilt.git._has_branch').return_value = False
     mocker.patch('gilt.git._has_tag').return_value = False
@@ -214,18 +244,92 @@ def test_has_version(temp_dir):
     repo = 'https://github.com/retr0h/ansible-etcd.git'
     destination = os.path.join(temp_dir.strpath, name)
     git.clone(name, repo, destination)
-    os.chdir(destination)
+    git_env = {
+        "GIT_WORK_TREE": destination,
+        "GIT_DIR": os.path.join(destination, ".git"),
+        "GIT_OBJECT_DIRECTORY": os.path.join(destination, ".git", "objects")
+    }
     # _has_branch tests
-    assert git._has_branch('master')
-    assert not git._has_branch('1.1')
-    assert not git._has_branch('888ef7b')
+    assert git._has_branch('master', env=git_env)
+    assert not git._has_branch('1.1', env=git_env)
+    assert not git._has_branch('888ef7b', env=git_env)
 
     # _has_tag tests
-    assert not git._has_tag('master')
-    assert git._has_tag('1.1')
-    assert not git._has_tag('888ef7b')
+    assert not git._has_tag('master', env=git_env)
+    assert git._has_tag('1.1', env=git_env)
+    assert not git._has_tag('888ef7b', env=git_env)
 
     # _has_commit tests
-    assert not git._has_commit('master')
-    assert not git._has_commit('1.1')
-    assert git._has_commit('888ef7b')
+    assert not git._has_commit('master', env=git_env)
+    assert not git._has_commit('1.1', env=git_env)
+    assert git._has_commit('888ef7b', env=git_env)
+
+
+def test_git_env():
+    result = git._git_env("temp_dir")
+
+    assert result == {
+        "GIT_WORK_TREE": "temp_dir",
+        "GIT_DIR": os.path.join("temp_dir", ".git"),
+        "GIT_OBJECT_DIRECTORY": os.path.join("temp_dir", ".git", "objects")
+    }
+
+
+def test_do_overlay_with_clone_extract(mocker, tmpdir):
+    clone = mocker.patch("gilt.git.clone")
+    extract = mocker.patch("gilt.git.extract")
+    overlay = mocker.patch("gilt.git.overlay")
+    config = mocker.MagicMock()
+    config.dst = "dest_dir"
+    mocker.patch("fasteners.InterProcessLock").return_value = \
+        mocker.MagicMock()
+    mocker.patch("os.path.exists").return_value = False
+    git.do_overlay(config)
+    clone.assert_called_once()
+    extract.assert_called_once()
+    overlay.assert_not_called()
+
+
+def test_do_overlay_with_clone_overlay(mocker, tmpdir):
+    clone = mocker.patch("gilt.git.clone")
+    extract = mocker.patch("gilt.git.extract")
+    overlay = mocker.patch("gilt.git.overlay")
+    config = mocker.MagicMock()
+    config.dst = ""
+    mocker.patch("fasteners.InterProcessLock").return_value = \
+        mocker.MagicMock()
+    mocker.patch("os.path.exists").return_value = False
+    git.do_overlay(config)
+    clone.assert_called_once()
+    extract.assert_not_called()
+    overlay.assert_called_once()
+
+
+def test_do_overlay_with_noclone_extract(mocker, tmpdir):
+    clone = mocker.patch("gilt.git.clone")
+    extract = mocker.patch("gilt.git.extract")
+    overlay = mocker.patch("gilt.git.overlay")
+    config = mocker.MagicMock()
+    config.dst = "dest_dir"
+    mocker.patch("fasteners.InterProcessLock").return_value = \
+        mocker.MagicMock()
+    mocker.patch("os.path.exists").return_value = True
+    git.do_overlay(config)
+    clone.assert_not_called()
+    extract.assert_called_once()
+    overlay.assert_not_called()
+
+
+def test_do_overlay_with_noclone_overlay(mocker, tmpdir):
+    clone = mocker.patch("gilt.git.clone")
+    extract = mocker.patch("gilt.git.extract")
+    overlay = mocker.patch("gilt.git.overlay")
+    config = mocker.MagicMock()
+    config.dst = ""
+    mocker.patch("fasteners.InterProcessLock").return_value = \
+        mocker.MagicMock()
+    mocker.patch("os.path.exists").return_value = True
+    git.do_overlay(config)
+    clone.assert_not_called()
+    extract.assert_not_called()
+    overlay.assert_called_once()